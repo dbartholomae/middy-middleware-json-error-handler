430a9b2546f9e13cd4640f5096463594e3e2a341
Daniel Bartholomae <daniel@bartholomae.name>
2022-02-26 13:06:36 +0100

    chore: update typedoc and semantic-release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-/** @ignore */
 /* istanbul ignore file */
-/** An additional comment to make sure Typedoc attributes the comment above to the file itself */
 import middleware from './JSONErrorHandlerMiddleware'
 export default middleware
 export * from './JSONErrorHandlerMiddleware'
