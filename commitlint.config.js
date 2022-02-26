2d3bda385fb36a80122ab800fdbb56e4097a780e
Daniel Bartholomae <daniel@bartholomae.name>
2022-02-26 19:15:31 +0100

    chore: allow dependabot commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['build']]
+    'scope-enum': [2, 'always', ['build', 'deps-dev']]
   }
 }
