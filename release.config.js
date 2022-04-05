bc95db8e64c0e03a0a8d5714623a17dbc8a2b8b6
Daniel Bartholomae <daniel@bartholomae.name>
2022-04-05 22:30:21 +0200

    chore: automatically create releases in GitHub and close issues

--- release.config.js
+++ release.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
+    '@semantic-release/github',
     ['@semantic-release/git', {
       assets: ['CHANGELOG.md', 'docs/**/*', 'package.json'],
       // eslint-disable-next-line no-template-curly-in-string
