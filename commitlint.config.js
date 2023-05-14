7fe1c1790bb2dbf42bf0c01f88b2a23f041a7514
Daniel Bartholomae <daniel@bartholomae.name>
2023-05-14 21:07:39 +0200

    chore: update commitlint and allow long bodies

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['build', 'deps-dev', 'deps']]
+    'scope-enum': [2, 'always', ['build', 'deps-dev', 'deps']],
+    'body-max-line-length': [0]
   }
 }
