309084dc6067400fd915200f69aac1a3b9ff140d
asargento <antonio.sargento@gmail.com>
2023-07-23 10:04:25 +0100

    feat: allow to expose internal errors via expose property
    BREAKING CHANGE: errors with the `expose` property set to a truthy
    value will be exposed even if their status code is >= 500

--- src/JSONErrorHandlerMiddleware.spec.ts
+++ src/JSONErrorHandlerMiddleware.spec.ts
@@ -64,7 +64,7 @@ describe('JSONErrorHandlerMiddleware', () => {
       })
     })
 
-    describe('with errors wit status code 500', () => {
+    describe('with errors with status code 500', () => {
       const statusCode = 500
 
       it('sets the response status code to 500', async () => {
@@ -94,6 +94,97 @@ describe('JSONErrorHandlerMiddleware', () => {
           message: 'Internal server error',
           statusCode: 500
         })
+      })
+    })
+  })
+
+  describe('with errors with expose property', () => {
+    it('returns the error when expose is true and the status is lower than 500', async () => {
+      const request = {
+        callback: jest.fn(),
+        context: {} as Context,
+        error: createHttpError(400, 'Oops', { expose : true}),
+        event: {},
+        internal: {},
+        response: {}
+      }
+      await JSONErrorHandlerMiddleware().onError(request)
+      expect(JSON.parse((request.response as any).body)).toEqual({
+        message: 'Oops',
+        name: 'BadRequestError'
+      })
+    })
+
+    it('returns the error 500 when expose is false and the status is lower than 500', async () => {
+      const request = {
+        callback: jest.fn(),
+        context: {} as Context,
+        error: createHttpError(400, 'Oops', { expose : false}),
+        event: {},
+        internal: {},
+        response: {}
+      }
+      await JSONErrorHandlerMiddleware().onError(request)
+      expect(JSON.parse((request.response as any).body)).toEqual({
+        message: 'Internal server error',
+        statusCode: 500
+      })
+    })
+
+    it('returns the error with status greater than 500 when expose is true', async () => {
+      const request = {
+        callback: jest.fn(),
+        context: {} as Context,
+        error: createHttpError(504, 'Oops', { expose : true}),
+        event: {},
+        internal: {},
+        response: {}
+      }
+      await JSONErrorHandlerMiddleware().onError(request)
+      expect(JSON.parse((request.response as any).body)).toEqual({
+        message: 'Oops',
+        name: 'GatewayTimeoutError',
+      })
+    })
+
+    it('returns the error with status 500 when any error than HttpError is sent with statusCode defined', async () => {
+      // tslint:disable-next-line:max-classes-per-file
+      class CustomError extends Error {
+        statusCode = 504
+      };
+      const request = {
+        callback: jest.fn(),
+        context: {} as Context,
+        error: new CustomError('Oops'),
+        event: {},
+        internal: {},
+        response: {}
+      }
+      await JSONErrorHandlerMiddleware().onError(request)
+      expect(JSON.parse((request.response as any).body)).toEqual({
+        message: 'Internal server error',
+        statusCode: 500
+      })
+    })
+
+    it('returns the error when any error than HttpError is sent with statusCode lower than 500', async () => {
+      // tslint:disable-next-line:max-classes-per-file
+      class CustomError extends Error {
+        statusCode = 404
+      };
+      const request = {
+        callback: jest.fn(),
+        context: {} as Context,
+        error: new CustomError('Oops'),
+        event: {},
+        internal: {},
+        response: {}
+      }
+      await JSONErrorHandlerMiddleware().onError(request)
+      expect(JSON.parse((request.response as any).body)).toEqual({
+        message: 'Oops',
+        name: 'Error',
+        statusCode: 404
       })
     })
   })
